3285481255761a5fe3ae89f86c1a9d0477f7797a
s.pavlinina <sonya.pav25@gmail.com>
2022-03-21 23:00:33 +0300

    Upd: Results descriptions

--- backend/src/main/java/com/clothesshop/backend/enums/ResultEnum.java
+++ backend/src/main/java/com/clothesshop/backend/enums/ResultEnum.java
@@ -6,22 +6,22 @@ import lombok.Getter;
 @Getter
 public enum ResultEnum {
 
-  PARAM_ERROR(1, "Parameter Error!"),
-  ITEM_NOT_EXIST(10, "Item does not exit!"),
-  ITEM_NOT_ENOUGH(11, "Not enough items in stock!"),
-  ITEM_STATUS_ERROR(12, "Status is incorrect!"),
-  ITEM_OFF_SALE(13, "Item is off sale!"),
-  ITEM_NOT_IN_CART(14, "Item is not in the cart!"),
-  CART_CHECKOUT_SUCCESS(20, "Checkout successfully! "),
+  PARAM_ERROR(1, "Parameter error"),
+  ITEM_NOT_EXIST(10, "Item does not exit"),
+  ITEM_NOT_ENOUGH(11, "Not enough items in the stock"),
+  ITEM_STATUS_ERROR(12, "Status is incorrect"),
+  ITEM_OFF_SALE(13, "Item is off sale"),
+  ITEM_NOT_IN_CART(14, "Item is not in the cart"),
+  CART_CHECKOUT_SUCCESS(20, "Checkout successfully"),
 
-  CATEGORY_NOT_FOUND(30, "Category does not exit!"),
+  CATEGORY_NOT_FOUND(30, "Category does not exit"),
 
-  ORDER_NOT_FOUND(60, "Order does not exit!"),
-  ORDER_STATUS_ERROR(61, "Status is not correct"),
+  ORDER_NOT_FOUND(60, "Order does not exist"),
+  ORDER_STATUS_ERROR(61, "Status is incorrect"),
 
 
   VALID_ERROR(50, "Wrong information"),
-  USER_NOT_FOUND(40, "User not found!");
+  USER_NOT_FOUND(40, "User not found");
 
   private Integer code;
 
